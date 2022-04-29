55de590f589cfdb39961059bfad892e2d3e5d0a3
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-04-29 00:05:42 -0500

    Adding class and spiderman test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-  test("Case 1 Dummy", () => {
-    const resultOfSomething = 9 + 9;
-    expect(resultOfSomething).toBe(18);
-  });
-});
+const Spiderman = require('./../app/spiderman')
+
+describe("Unit test for spiderman class", () => {
+  test('1) Create an spiderman object', () => {
+    const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield", 2, "Sony")
+
+    expect(andrewGarfield.name).toBe("Spiderman Sony")
+    expect(andrewGarfield.age).toBe(31)
+    expect(andrewGarfield.actor).toBe("Andrew Garfield")
+    expect(andrewGarfield.movies).toBe(31)
+    expect(andrewGarfield.studio).toBe("Sony")
+  })
+})
