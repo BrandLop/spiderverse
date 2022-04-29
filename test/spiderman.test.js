3284dd2bb09886680eebfee0a6199fa22edffbab
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-04-28 23:31:51 -0500

    Fixiing bugs in dummy test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,6 @@
 describe("Test Suite Dummy Description", () => {
   test("Case 1 Dummy", () => {
-    const resultOfSomething = 1 + 2;
+    const resultOfSomething = 9 + 9;
     expect(resultOfSomething).toBe(18);
   });
 });
